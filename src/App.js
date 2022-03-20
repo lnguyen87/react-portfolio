3dee350a338fbc5dcda9b8ef4ee822bc19054cb2
lnguyen87 <luc_nguyen@outlook.com>
2022-03-19 22:44:05 -0600

    moved header to nav, replaced header component with contact component, fixed page links

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
-import React from 'react';
-import Header from './components/Header';
+import React, { useState } from 'react';
+import ContactForm from './components/Contact';
 import Navigation from './components/Navigation';
 import About from './components/About';
 import Footer from './components/Footer';
-import Project from './components/Project'
+
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'projects',
+      description: 'Collection of GitHub projects',
+    },
+    {
+      name: 'resume',
+      description: 'Most recent resume',
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
     <div className="bg-gray-300 text-black h-screen font-sans">
-      <Navigation></Navigation>
+      <Navigation
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}
+      contactSelected={contactSelected}
+      setContactSelected={setContactSelected}
+      ></Navigation>
       <main>
-        <Header></Header>
-        <About></About>
-        <Project></Project>
         <Footer></Footer>
+
+        {!contactSelected ? (
+          <>
+            <About currentCategory={currentCategory}></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,45 @@
 import React from 'react';
 
-function Navigation() {
+function Navigation(props) {
+    const {
+        categories = [],
+        setCurrentCategory,
+        contactSelected,
+        currentCategory,
+        setContactSelected,
+    } = props;
+
     return (
         <header>
-
+            <nav className="bg-white text-black">
+                <ul className="flex flex-wrap justify-between align-center list-none pt-6 pb-6 px-24 font-bold text-2xl">
+                    <h1 className="text-4xl font-bold">
+                        <a data-testid="link" href="/">Luc Nguyen</a>
+                    </h1>
+                    <li className="pl-80">
+                        <a data-testid="about" href="#about" onClick={() => setContactSelected(false)} className="hover:text-orange-500">About Me</a>
+                    </li>
+                    <li className={`${contactSelected && 'navActive'}`}>
+                    <span onClick={() => setContactSelected(true)}>Contact</span>
+                    </li>
+                    {categories.map((category) => (
+                        <li
+                        className={`mx-1 ${
+                            currentCategory.name === category.name && !contactSelected && 'navActive'
+                            }`}
+                        key={category.name}
+                        >
+                        <span
+                            onClick={() => {
+                            setCurrentCategory(category);
+                            setContactSelected(false);
+                            }}
+                        >
+                        </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
         </header>
     );
 }
