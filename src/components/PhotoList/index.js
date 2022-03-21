d94801ac5bdf1103e1c80ee1ca2cfde81b59c185
lnguyen87 <luc_nguyen@outlook.com>
2022-03-21 17:48:00 -0600

    added github and deployed links for projects in Modal component

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -10,31 +10,43 @@ const PhotoList = ({ category }) => {
           name: 'Last Minute',
           category: 'project',
           description: 'Date night event planner',
+          githubLink: 'https://github.com/lnguyen87/lastMinute',
+          deployedLink: 'https://hone12345678.github.io/lastMinute/',
       },
       {
           name: 'Troubleshot',
           category: 'project',
           description: 'Personal database for keeping track of questions and answers',
+          githubLink: 'https://github.com/lnguyen87/Troubleshot',
+          deployedLink: 'https://troubleshot.herokuapp.com/',
       },
       {
           name: 'Weather Dashboard',
           category: 'project',
           description: 'Weather tracker',
+          githubLink: 'https://github.com/lnguyen87/weather-dashboard',
+          deployedLink: 'https://lnguyen87.github.io/weather-dashboard/',
       },
       {
           name: 'Workday Scheduler',
           category: 'project',
           description: 'Simple workday scheduler',
+          githubLink: 'https://github.com/lnguyen87/work-day-scheduler',
+          deployedLink: 'https://lnguyen87.github.io/work-day-scheduler/',
       },
       {
           name: 'Password Generator',
           category: 'project',
           description: 'Password generator',
+          githubLink: 'https://github.com/lnguyen87/generate-password',
+          deployedLink: 'https://lnguyen87.github.io/generate-password/',
       },
       {
           name: 'Code Quiz',
           category: 'project',
-          description: 'Simple code quiz'
+          description: 'Simple code quiz',
+          githubLink: 'https://github.com/lnguyen87/code-quiz',
+          deployedLink: 'https://lnguyen87.github.io/code-quiz/',
       }
     ]);
 
