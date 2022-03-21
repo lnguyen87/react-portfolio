8ffbff4502edd2410e1ff27abe7ac7bded30920c
lnguyen87 <luc_nguyen@outlook.com>
2022-03-21 17:17:07 -0600

    fixed href link to portfolio

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -19,7 +19,7 @@ function Navigation(props) {
             <nav className="bg-white text-black">
                 <ul className="flex flex-wrap justify-between align-center list-none mt-6 pb-6 mx-24 font-bold text-4xl font-shalimar">
                     <h1 className="text-5xl font-windsong">
-                        <a data-testid="link" href="/" className="hover:opacity-60">Luc Nguyen</a>
+                        <a data-testid="link" href="/react-portfolio" className="hover:opacity-60">Luc Nguyen</a>
                     </h1>
                     <li className="ml-80">
                         <a data-testid="about" href="#about" onClick={() => setContactSelected(false)} className="hover:text-orange-500">About Me</a>
